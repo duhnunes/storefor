15c826ab6c4f5ed147cdb118c56eca425f81d1de
DuHNunes <duhnunes.dev@gmail.com>
2023-08-21 22:39:20 -0300

    [Add]Page Products (dev)

---
+++ js/menus.js
@@ -0,0 +1,7 @@
+// DROPDOWN PERSISTENCE
+const dropdown = document.querySelector('.dropdown');
+const show = document.querySelector('.dropdown__content');
+
+dropdown.addEventListener('click', () => {
+    show.classList.toggle('show');
+});
