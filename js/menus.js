8d2a7ff5a4951bf274c680189ae9d9a81fbca946
DuHNunes <duhnunes.dev@gmail.com>
2023-08-25 16:40:34 -0300

    [REFAC] Rework navbar Menu

--- js/menus.js
+++ js/menus.js
@@ -1,9 +1,30 @@
 // DROPDOWN PERSISTENCE
-const dropdown = document.querySelector('.dropdown');
-const show = document.querySelector('.dropdown__content');
+const menu = document.querySelector('.menu__shop');
 
-dropdown.addEventListener('click', () => {
-    show.classList.toggle('show');
-});
+function userMenu(){
+    const user = document.querySelector(".user-pic");
+    const navlink = document.querySelector('.nav-link');
+
+    let navLink = [1,2,3,4,5];
+    for(let pos in navLink){
+        console.log(navLink[pos]);
+    }
+
+    if(user.onclick){
+        document.querySelector(".dropdown-wrap").classList.toggle('show');
+    }
+}
+window.onclick = function(e){
+    if(!e.target.matches('.user-pic') && !e.target.matches('.user-pic img')){
+        var dropdowns = document.getElementsByClassName("dropdown-wrap");
+        var i;
+        for (i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
 
 // FILTERS PERSISTENCE
