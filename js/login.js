535d9804233a93add6aa7ba921e00355008824d5
DuHNunes <duhnunes.dev@gmail.com>
2023-09-01 17:24:42 -0300

    Eyes change with click

--- js/login.js
+++ js/login.js
@@ -1,23 +1,31 @@
 // CHANGE LOGIN EYES WHEN CLICK
-
 const pw = document.getElementById('pw');
 const eye = document.querySelector(".pw-eye");
+const icon = document.querySelector('#change');
 eye.addEventListener('click', () => {
     if(pw.type == "text"){
         pw.type="password";
         pw.focus();
+        if(icon.classList.contains('fa-eye-slash')){
+            icon.classList.remove('fa-eye-slash');
+            icon.classList.add('fa-eye');
+        }
+        
     }else {
         pw.type="text";
         pw.focus();
+        if(icon.classList.contains('fa-eye')){
+            icon.classList.remove('fa-eye');
+            icon.classList.add('fa-eye-slash');
+
+        }
+        
     }
 })
+icon.classList.add('fa-solid fa-eye-slash');
+
 
 // VALIDATIONS EMAIL AND PASSWORD
-const createBallons = () => {
-    
-}
-
-
 const email = document.getElementById('email');
 
 const enter = document.querySelector('.btn-login');
@@ -33,4 +41,4 @@ enter.addEventListener('click', () => {
     }
 })
 
-// PASSWORD NEEDS
+// 
