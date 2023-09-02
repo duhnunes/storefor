812fd52d8630f126fcb91b61f6f9d43196be94eb
DuHNunes <duhnunes.dev@gmail.com>
2023-09-02 17:01:51 -0300

    Refac/Change layout to validation

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 // CHANGE LOGIN EYES WHEN CLICK
 const pw = document.getElementById('pw');
 const eye = document.querySelector(".pw-eye");
-const icon = document.querySelector('#change');
+const icon = document.querySelector('.pw-eye i');
 eye.addEventListener('click', () => {
     if(pw.type == "text"){
         pw.type="password";
@@ -10,19 +10,15 @@ eye.addEventListener('click', () => {
             icon.classList.remove('fa-eye-slash');
             icon.classList.add('fa-eye');
         }
-        
     }else {
         pw.type="text";
         pw.focus();
         if(icon.classList.contains('fa-eye')){
             icon.classList.remove('fa-eye');
             icon.classList.add('fa-eye-slash');
-
         }
-        
     }
 })
-icon.classList.add('fa-solid fa-eye-slash');
 
 
 // VALIDATIONS EMAIL AND PASSWORD
@@ -32,7 +28,7 @@ const enter = document.querySelector('.btn-login');
 enter.addEventListener('click', () => {
     if(email.value == ""){
         email.focus();
-        email.style.outline = "1px solid red";
+        email.style.outline = "2px solid red";
         alert('Digite seu email!');
     }else if(pw.value == ""){
         pw.focus();
