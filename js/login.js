02622fc78d5a4fa575f847d71ac92e7cdfbef81a
DuHNunes <duhnunes.dev@gmail.com>
2023-09-02 23:46:17 -0300

    Remake the layout validation

--- js/login.js
+++ js/login.js
@@ -20,21 +20,4 @@ eye.addEventListener('click', () => {
     }
 })
 
-
 // VALIDATIONS EMAIL AND PASSWORD
-const email = document.getElementById('email');
-
-const enter = document.querySelector('.btn-login');
-enter.addEventListener('click', () => {
-    if(email.value == ""){
-        email.focus();
-        email.style.outline = "2px solid red";
-        alert('Digite seu email!');
-    }else if(pw.value == ""){
-        pw.focus();
-        pw.style.outline = "1px solid red";
-        alert('Digite sua senha!');
-    }
-})
-
-// 
