2cb0890a8990a68ec3947a55be791a757f781c0c
DuHNunes <duhnunes.dev@gmail.com>
2023-09-19 14:02:07 -0300

    crud system with node.js

---
+++ js/register.js
@@ -0,0 +1,35 @@
+const createForm = document.getElementById('createForm');
+createForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const cpf = document.getElementById('cpf').value;
+    const birthday = document.getElementById('birthday').value;
+    const phone = document.getElementById('phone').value;
+    const remail = document.getElementById('remail').value;
+    const password = document.getElementById('pw').value;
+
+    const data = {
+        username,
+        cpf,
+        birthday,
+        phone,
+        remail,
+        password
+    };
+
+    const response = await fetch('./js/users.js', {
+        methor:'POST',
+        headers:{
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(data),
+    });
+
+    if(response.ok){
+        const userData = await response.json();
+        console.log('Usuário Cadastrado' + userData);
+    }else{
+        console.error('Error ao cadastrar usuário');
+    }
+})
---
+++ js/users.js
@@ -0,0 +1,74 @@
+const express = require("express")
+const server = express();
+const router = express.Router();
+const fs = require('fs');
+
+server.use(express.json({extended: true}));
+
+const readFile = () => {
+    const content = fs.readFileSync('./assets/users/data/users.json', 'utf-8');
+    return (JSON.parse(content));
+}
+const writeFile = (content) => {
+    const updateFile = JSON.stringify(content);
+    fs.writeFileSync('./assets/users/data/users.json', updateFile, 'utf-8');
+
+}
+
+// READ
+router.get('/', (req, res) => {
+    const content = readfile();
+    res.send(content);
+})
+
+// CREATE
+router.post('/', (req, res) => {
+    const {name, CPF, birthday, celphone, email, password} = req.body
+    const currentContent = readFile();
+    const id = Math.random().toString(32).substring(2, 9);
+    currentContent.push({id, name, CPF, birthday, celphone, email, password});
+    writeFile(currentContent);
+    res.send({id, name, CPF, birthday, celphone, email, password});
+})
+
+//UPDATE
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+
+    const {name, CPF, birthday, celphone, email, password} = req.body;
+
+    const currentContent = readFile();
+    const selectedItem = currentContent.findIndex((item) => item.id === id);
+
+    const {id: cId, name: cName, CPF: cCPF, birthday: cBirthday, celphone: cCelphone, email: cEmail, password: cPassword} = currentContent[selectedItem];
+
+    const newObject = {
+        id: cId,
+        name: name ? name: cName,
+        CPF: CPF ? CPF: cCPF,
+        birthday: birthday ? birthday: cBirthday,
+        celphone: celphone ? celphone: cCelphone,
+        email: email ? email: cEmail,
+        password: password ? password: cPassword,
+    }
+
+    currentContent[selectedItem] = newObject;
+    writeFile(currentContent);
+    res.send(newObject);
+})
+
+//DELETE
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    const currentContent = readFile();
+    const selectedItem = currentContent.findIndex((item) => item.id === id);
+    currentContent.splice(selectedItem, 1);
+    writeFile(currentContent);
+    res.send(true);
+})
+
+server.use(router);
+
+server.listen(3000, () => {
+    console.log('rodando servidor');
+})
